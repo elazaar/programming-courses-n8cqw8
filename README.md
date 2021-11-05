171b4c3b35539e249813c616df8c2877aa7b5833
elazaar <simo2002yamna@gmail.com>
2021-11-05 01:40:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-nz31va
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-nz31va)
---
+++ index.ts
@@ -0,0 +1,16 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+console.log('hello imran');
+console.log('merci');
+console.log('fuck off');
+console.log('bebe');
+let num1: number;
+let num2: number;
+num1 = 22;
+num2 = 88;
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num2 / num1);
+console.log(num1 * num2);
